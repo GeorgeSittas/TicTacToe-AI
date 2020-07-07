8f931161b3b4248637ce0622588fde30d5d0b7dc
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2020-07-07 21:08:25 +0300

    Update tic_tac_toe.c

--- src/tic_tac_toe.c
+++ src/tic_tac_toe.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-#include "../include/tic_tac_toe.h"
-#include "../include/board.h"
-#include "../include/utilities.h"
+#include "tic_tac_toe.h"
+#include "board.h"
+#include "utilities.h"
 
 void init_game(void) {
   init_board();
-  game.user_plays = (user_symbol() == CROSS); /* X plays first */
+  game.user_plays = (user_symbol() == CROSS); // X plays first
   game.turn_no = 1;
 }
 
-void next_turn(void) { game.user_plays ^= true, game.turn_no++; }
+void next_turn(void) { game.user_plays ^= 1, game.turn_no++; }
 
 void user_move(void) {
   printf("> "); skip_whitespace();
@@ -49,10 +49,11 @@ void user_move(void) {
 void computer_move(void) {
   int row, col;
 
-  /* Extra flag that's used to stop the search in case terminal */
-  /* states are reached early (in the first or second move) */
+  /* Extra flag that's used to stop the search in case terminal
+   * states are reached early (in the first or second move)
+   */
 
-  bool_t stop = false;
+  bool stop = false;
 
   minimax(true, &stop, turn_no(), &row, &col);
   if (!valid_indeces(row, col))
@@ -62,7 +63,7 @@ void computer_move(void) {
   set_board(row, col, comp_symbol());
 }
 
-int minimax(bool_t max_plays, bool_t *stop, int turn, int *row, int *col) {
+int minimax(bool max_plays, bool *stop, int turn, int *row, int *col) {
   if ( max_plays && winning_sequence_exists(comp_symbol())) return P_INF;
   if (!max_plays && winning_sequence_exists(user_symbol())) return M_INF;
   if (turn > MAX_TURNS)                                     return DRAW_VAL;
@@ -71,31 +72,35 @@ int minimax(bool_t max_plays, bool_t *stop, int turn, int *row, int *col) {
 
   if (max_plays) {
 
-    /* Initially, any move is the best move for the maximizing player */
+    // Initially, any move is the best move for the maximizing player
     int max_evaluation = M_INF;
 
-    /* Regard every possible move the maximizing player can */
-    /* play as a next game state */
+    /* Regard every possible move the maximizing player can
+     * play as a next game state
+     */
 
     for (int i = 0; i < SIZE; i++) {
       for (int j = 0; j < SIZE; j++) {
         if (is_board(i, j, EMPTY)) {
 
-          /* Simulate next game state to call minimax again and then undo it */
+          // Simulate next game state to call minimax again and then undo it
           set_board(i, j, comp_symbol());
           evaluation = minimax(false, stop, turn+1, row, col);
           set_board(i, j, EMPTY);
 
-          /* User may have a winning move that needs to be blocked (the stop */
-          /* flag gets triggered only on the first recursive call of minimax) */
+          /* User may have a winning move that needs to be blocked (the stop
+           * flag gets triggered only on the first recursive call of minimax)
+           */
 
-          if (*stop) return M_INF; /* The search stops here */
+          if (*stop) return M_INF; // The search stops here
 
           if (max_evaluation < evaluation) {
             max_evaluation = evaluation;
 
-            /* The best move is decided on the first call of minimax, and if */
-            /* a winning move is reached for the computer, the search stops */
+            /* The best move is decided on the first level of the minimax
+             * tree, and if a winning move is reached for the computer, the
+             * search stops
+             */
 
             if (turn == turn_no()) {
               *row = i, *col = j;
@@ -109,17 +114,18 @@ int minimax(bool_t max_plays, bool_t *stop, int turn, int *row, int *col) {
     return max_evaluation;
   }
 
-  /* Initially, any move is the worst move for the minimizing player */
+  // Initially, any move is the worst move for the minimizing player
   int min_evaluation = P_INF;
 
-  /* Regard every possible move the minimizing player can */
-  /* play as a next game state */
+  /* Regard every possible move the minimizing player can
+   * play as a next game state
+   */
 
   for (int i = 0; i < SIZE; i++) {
     for (int j = 0; j < SIZE; j++) {
       if (is_board(i, j, EMPTY)) {
 
-        /* Simulate next game state to call minimax again and then undo it */
+        // Simulate next game state to call minimax again and then undo it
         set_board(i, j, user_symbol());
         evaluation = minimax(true, stop, turn+1, row, col);
         set_board(i, j, EMPTY);
@@ -127,9 +133,10 @@ int minimax(bool_t max_plays, bool_t *stop, int turn, int *row, int *col) {
         if (min_evaluation > evaluation) {
           min_evaluation = evaluation;
 
-          /* If the user's next move is a winning one, then the computer */
-          /* needs to block it. For that purpose, the stop flag is activated */
-          /* and the indeces of that winning move are saved */
+          /* If the user's next move is a winning one, then the computer
+           * needs to block it. For that purpose, the stop flag is activated
+           * and the indeces of that winning move are saved
+           */
 
           if (turn == turn_no() - 1 && min_evaluation == M_INF) {
             *row = i, *col = j;
@@ -144,7 +151,7 @@ int minimax(bool_t max_plays, bool_t *stop, int turn, int *row, int *col) {
   return min_evaluation;
 }
 
-bool_t winning_sequence_exists(char symbol) {
+bool winning_sequence_exists(char symbol) {
   for (int i = 0; i < SIZE; i++) {
     if (is_board(i, 0, symbol)
      && is_board(i, 1, symbol)
@@ -166,8 +173,9 @@ bool_t winning_sequence_exists(char symbol) {
   return false;
 }
 
-/* Checks whether the game has ended, in which case the */
-/* appropriate signalling (state) value is returned */
+/* Checks whether the game has ended, in which case the
+ * appropriate signalling (state) value is returned
+ */
 
 state_t game_state(void) {
   if (winning_sequence_exists(user_symbol())) return WON;
