4458bc83aef2bd40c93085bc6cebe37f608684b4
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2020-07-07 21:34:10 +0300

    Update utilities.h

--- include/utilities.h
+++ include/utilities.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #define SEC_IN_MICROSEC 1000000
-#define MOVE_DELAY SEC_IN_MICROSEC /* Computer has a 1 sec move delay */
+#define MOVE_DELAY SEC_IN_MICROSEC // Computer has a 1 sec move delay
 
 #include "types.h"
 
 void process_CLA(int, char **);
 
-bool_t valid_index(int);
-bool_t valid_indeces(int, int);
+bool valid_index(int);
+bool valid_indeces(int, int);
 
 void set_board(int, int, char);
 char get_board(int, int);
-bool_t is_board(int, int, char);
+bool is_board(int, int, char);
 
 char user_symbol(void);
 char comp_symbol(void);
-bool_t user_plays(void);
+bool user_plays(void);
 int turn_no(void);
 
 void skip_whitespace(void);
